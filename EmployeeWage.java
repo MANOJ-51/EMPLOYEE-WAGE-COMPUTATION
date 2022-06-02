386b75904e98a614725a66cdf704846c1bb978c8
MANOJ-51 <m4anoj51@gmail.com>
2022-06-02 23:49:33 +0530

    added:uc2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,21 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+        //constant variables
         int isPresent = 1;
+        int wagePerHour=20;
+        //temporary variables
+        int employeeHours=0;
+        int employeeWage=0;
+        /*finding employee is present or not*/
         int attandance = (int) Math.floor(Math.random() * 2);
         if (isPresent == attandance) {
-            System.out.println("EMPLOYEE IS PRESENT");
+            employeeHours=8;
         } else {
-            System.out.println("EMPLOYEE IS ABSENT");
+            employeeHours=0;
         }
+        //calclulating  employee dialy wage
+        employeeWage=employeeHours*wagePerHour;
+        System.out.println("EMPLOYEE WAGE: "+employeeWage);
     }
 }
