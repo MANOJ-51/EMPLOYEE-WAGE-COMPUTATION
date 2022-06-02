84ceb9711c32a872a0e0289edea08a8fc6aaf206
MANOJ-51 <m4anoj51@gmail.com>
2022-06-03 00:00:38 +0530

    added:uc5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,4 +1,3 @@
-
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
@@ -6,27 +5,30 @@ public class EmployeeWage {
         final int isPresent = 1;
         final int wagePerHour = 20;
         final int isParttime = 2;
+        final int totalWorkingDays = 20;
         //temporary variables
         int employeeHours = 0;
         int employeeWage = 0;
+        int totalEmployeeWage = 0;
         /*finding employee is present or not using switch case*/
-        int attandance = (int) Math.floor(Math.random() * 3);
-        switch (attandance) {
-            case isPresent:
-                employeeHours = 8;
-                System.out.println("EMPLOYEE IS FULLTIME..");
-                break;
-            case isParttime:
-                employeeHours = 4;
-                System.out.println("EMPLOYEE IS PARTTIME..");
-                break;
-            default:
-                employeeHours = 0;
-                System.out.println("EMPLOYEE IS ABSENT...");
+        for (int day = 1; day < totalWorkingDays; day++) {
+            int attandance = (int) Math.floor(Math.random() * 3);
+            switch (attandance) {
+                case isPresent:
+                    employeeHours = 8;
+                    break;
+                case isParttime:
+                    employeeHours = 4;
+                    break;
+                default:
+                    employeeHours = 0;
+            }
+            //calclulating  employee total wage
+            employeeWage = employeeHours * wagePerHour;
+            totalEmployeeWage = totalEmployeeWage + employeeWage;
+            System.out.println("EMPLOYEE DAY .."+day+"EMPLOYEE INCOME."+employeeWage);
         }
-        //calclulating  employee dialy wage
-        employeeWage=employeeHours*wagePerHour;
-        System.out.println("EMPLOYEE WAGE: "+employeeWage);
+        System.out.println("EMPLOYE WAGE FOR MONTH : " + totalEmployeeWage);
     }
 }
 
