c78b1a13500db6fa7387ff0489b2cf299a6cf3ba
MANOJ-51 <m4anoj51@gmail.com>
2022-06-06 19:46:21 +0530

    resolved class method object name

--- MainClassEmployeeWage.java
+++ MainClassEmployeeWage.java
@@ -2,7 +2,7 @@ package com.employeWage;
 
 public class MainClassEmployeeWage {
     public static void main(String[] args) {
-       Uc7ClassMethod myObject =new Uc7ClassMethod();
+       EmployeeWage myObject =new EmployeeWage();
        myObject.computationOfEmployeeWage();
     }
 }
