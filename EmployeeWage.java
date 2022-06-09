fb39379bfa5939aad22ad89ffd207df5fba4e1ce
MANOJ-51 <m4anoj51@gmail.com>
2022-06-10 00:22:47 +0530

    added : dialy wage storage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,20 +1,47 @@
-public class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
-        //constant variables
-        final int isPresent = 1;
-        final int wagePerHour = 20;
-        final int isParttime = 2;
-        final int totalWorkingDays = 20;
-        int maximumWorkingHours=100;
+package com.employeWage;
+
+
+import java.util.ArrayList;
+
+//uc13 storing dialy wage along with total wage
+public class EmployeeWage implements InterfaceCompanyEmployeeWage {
+
+    //constant variables
+    static final int isPresent = 1;
+    static final int isParttime = 2;
+
+
+    //initializing arrayLlist
+    static ArrayList<CompanyEmployeeWage> companyEmployeeWageArray = new ArrayList<CompanyEmployeeWage>();
+
+
+    //adding company details into array
+    public void addCompanyEmployeeWage(String companyName, int totalWorkingDays, int maximumWorkingHours, int wagePerHour) {
+        CompanyEmployeeWage companyEmployeeWage = new CompanyEmployeeWage(companyName, totalWorkingDays, maximumWorkingHours, wagePerHour);
+        companyEmployeeWageArray.add(companyEmployeeWage);
+    }
+
+    //setting total employee wage in an array for multiple companies
+    public void computationOfEmployeeWage() {
+        for (int i = 0; i < companyEmployeeWageArray.size(); i++) {
+            CompanyEmployeeWage companyEmployeeWage = companyEmployeeWageArray.get(i);
+            companyEmployeeWage.setTotalEmployeeWage(this.computationOfEmployeeWage(companyEmployeeWage));
+            System.out.println(companyEmployeeWage);
+        }
+    }
+
+
+    //computation of employee wage
+    public int computationOfEmployeeWage(CompanyEmployeeWage companyEmployeeWage) {
+
         //temporary variables
         int employeeHours = 0;
         int employeeWage = 0;
-        int totalEmployeeWage = 0;
-        int totalEmployeeHours=0;
-        int numberOfDays=0;
+
+        int totalEmployeeHours = 0;
+        int numberOfDays = 0;
         /*finding employee is present or not using switch case*/
-        while (totalEmployeeHours<maximumWorkingHours && numberOfDays<totalWorkingDays) {
+        while (totalEmployeeHours <= companyEmployeeWage.maximumWorkingHours && numberOfDays < companyEmployeeWage.totalWorkingDays) {
             numberOfDays++;
             int attandance = (int) Math.floor(Math.random() * 3);
             switch (attandance) {
@@ -28,12 +55,18 @@ public class EmployeeWage {
                     employeeHours = 0;
             }
             //calclulating  employee total wage
-            totalEmployeeHours+=employeeHours;
-            employeeWage = employeeHours * wagePerHour;
-            totalEmployeeWage = totalEmployeeWage + employeeWage;
-            System.out.println("EMPLOYEE DAY .." + numberOfDays + "EMPLOYEE INCOME." + employeeWage);
+            totalEmployeeHours += employeeHours;
+            employeeWage = employeeHours * companyEmployeeWage.wagePerHour;
+            companyEmployeeWage.totalEmployeeWage = companyEmployeeWage.totalEmployeeWage + employeeWage;
+
+            //uc13 storing dialy wage along with total wage
+            System.out.println(companyEmployeeWage.companyName + " DAY " + numberOfDays + " WAGE " + employeeWage + " EMPLOYEE HOURS " + employeeHours);
         }
-        System.out.println("EMPLOYEE TOTAL WAGE: " + totalEmployeeWage);
+        return companyEmployeeWage.totalEmployeeWage; //returning to total employee wage
+
     }
+
+
 }
+
 
---
+++ MainClassEmployeeWage.java
@@ -0,0 +1,16 @@
+package com.employeWage;
+
+public class MainClassEmployeeWage {
+    public static void main(String[] args) {
+
+        //adding company details by creating objects
+        InterfaceCompanyEmployeeWage object = new EmployeeWage();   //creating obj of sub class with reference of super class interface
+
+        //adding comapnies to array
+        object.addCompanyEmployeeWage("INFOSYS", 20, 100, 40);
+        object.addCompanyEmployeeWage("TCS", 25, 80, 20);
+
+        //calling method for computation
+        object.computationOfEmployeeWage();
+    }
+}
